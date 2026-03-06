725b29f3378ff9b278e45e2866996c75990345c2
Rodrig0304 <129132001+Rodrig0304@users.noreply.github.com>
2026-03-06 09:51:09 -0600

    textos

--- src/components/IntroFooter.jsx
+++ src/components/IntroFooter.jsx
@@ -1,35 +1,37 @@
 import { useLanguage } from '../context/LanguageContext'
 import './IntroFooter.css'
 
-const FOOTER_BLOCKS = [
-  {
-    titleKey: 'intro.footer.techTitle',
-    items: ['intro.footer.tech1', 'intro.footer.tech2', 'intro.footer.tech3', 'intro.footer.tech4'],
-  },
-  {
-    titleKey: 'intro.footer.solutionsTitle',
-    items: ['intro.footer.sol1', 'intro.footer.sol2', 'intro.footer.sol3', 'intro.footer.sol4'],
-  },
-]
-
 export function IntroFooter() {
   const { t } = useLanguage()
 
   return (
     <footer className="intro-footer">
       <div className="intro-footer-inner">
-        {FOOTER_BLOCKS.map((block) => (
-          <div key={block.titleKey} className="intro-footer-block">
-            <h4 className="intro-footer-title">{t(block.titleKey)}</h4>
-            <ul className="intro-footer-list">
-              {block.items.map((itemKey) => (
-                <li key={itemKey}>
-                  <span>{t(itemKey)}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        {/* Tecnología e innovación */}
+        <div className="intro-footer-block">
+          <h4 className="intro-footer-title">{t('intro.footer.techTitle')}</h4>
+          <ul className="intro-footer-list">
+            {['tech1', 'tech2', 'tech3', 'tech4'].map((key) => (
+              <li key={key}>{t(`intro.footer.${key}`)}</li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Tu siguiente paso */}
+        <div className="intro-footer-block intro-footer-block--cta">
+          <h4 className="intro-footer-title">{t('intro.cta.heading')}</h4>
+          <p className="intro-footer-cta-text">{t('intro.cta.text')}</p>
+        </div>
+
+        {/* Nuestras soluciones */}
+        <div className="intro-footer-block">
+          <h4 className="intro-footer-title">{t('intro.footer.solutionsTitle')}</h4>
+          <ul className="intro-footer-list">
+            {['sol1', 'sol2', 'sol3', 'sol4'].map((key) => (
+              <li key={key}>{t(`intro.footer.${key}`)}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   )
